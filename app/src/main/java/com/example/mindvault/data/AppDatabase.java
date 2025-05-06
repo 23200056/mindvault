b49d8da88532d01327b92b513b0fa868ebd2c76c
Edouard Ybañez <145130147+Shifts334@users.noreply.github.com>
2025-05-06 18:04:33 +0800

    Login Big Fix

--- app/src/main/java/com/example/mindvault/data/AppDatabase.java
+++ app/src/main/java/com/example/mindvault/data/AppDatabase.java
@@ -1,9 +1,34 @@
 package com.example.mindvault.data;
 
+import android.content.Context;
+
 import androidx.room.Database;
+import androidx.room.Room;
 import androidx.room.RoomDatabase;
+
+import com.example.mindvault.ui.forgotpass.ForgotPasswordPage;
 
 @Database(entities = {Note.class}, version = 1)
 public abstract class AppDatabase extends RoomDatabase {
+
+    private static volatile AppDatabase INSTANCE;
+
+    public abstract UserDao userDao();
+
+    public static AppDatabase getInstance(Context context) {
+        if (INSTANCE == null) {
+            synchronized (AppDatabase.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(
+                            context.getApplicationContext(),
+                            AppDatabase.class,
+                            "mindvault_db"
+                    ).build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+
     public abstract NoteDao noteDao();
 }
